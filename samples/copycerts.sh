2273499ee73243a19adbc9c7a0a660d854bb9a1c
Raghuram Devarakonda <draghuram@gmail.com>
2019-12-19 19:29:44 -0500

    Fix the env variable name of kubedrutil image.
    Also, switched to kubedrutil:0.0.2 that contains fixes for permissions.

--- samples/copycerts.sh
+++ samples/copycerts.sh
@@ -5,4 +5,5 @@ set -ex
 cp /var/lib/minikube/certs/etcd/ca.crt /tmp
 cp /var/lib/minikube/certs/apiserver-etcd-client.crt /tmp/client.crt
 cp /var/lib/minikube/certs/apiserver-etcd-client.key /tmp/client.key
+chmod 777 /tmp/ca.crt /tmp/client.crt /tmp/client.key
 
